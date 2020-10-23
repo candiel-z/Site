db6db9a6f2386aeafccec887e36e528f17212161
candiel-z <candiel-z@yahoo.com>
2020-10-23 13:41:07 +0300

    Orders added

--- Site/app/database.py
+++ Site/app/database.py
@@ -9,45 +9,55 @@ class Database(object):
         
 
         if Config.DEBUG:
-            self._execute("""
-                drop table if exists Orders
-            """)
+            self._execute('drop table if exists Clients;')
+            self._execute('drop table if exists Orders;')
 
         self._execute("""
-            create table if not exists Orders (
-                id integer primary key,
-                name text not null,
-                surname text not null,
-                mobile_number text not null unique
+            create table if not exists Clients (
+                    ididentifier integer unique,
+                    first_name text not null,
+                    second_name text not null,
+                    mobile_number text not null unique
             );
         """)
+        self._execute("""
+            create table if not exists Orders (
+                    ididentifier integer,
+                    product_name text not null,
+                    product_amount integer not null
+            );
 
-    def _connect(self):
-            """"""
+        """)
 
-            try:
-                connection = sqlite3.connect(Config.DATABASE)
-                cursor = connection.cursor()
-            except sqlite3.Error as e:
-                print('database._connect |', e)
-                connection.close()
-            else:
-                return connection, cursor
-
-    def add(self, name: str, surname: str, mobile_number: str) -> None:
+    def add(self, data) -> None:
         """"""
         
+        ididentifier = data.get('ididentifier')
+        first_name = data.get('first_name')
+        second_name = data.get('second_name')
+        mobile_number = data.get('mobile_number')
+
+        product_names = data.getlist('product_name')
+        product_amount = data.getlist('product_amount')
+
         self._execute(f"""
-                insert into Orders
-                (name, surname, mobile_number)
+                insert into Clients
+                (ididentifier, first_name, second_name, mobile_number)
                 values
-                ('{name}','{surname}','{mobile_number}')
+                ('{ididentifier}','{first_name}','{second_name}','{mobile_number}')
         """)
+        for i in range(len(product_names)):
+            self._execute(f"""
+                insert into Orders
+                (ididentifier, product_name, product_amount)
+                values
+                ('{ididentifier}','{product_names[i]}','{product_amount[i]}')
+            """)
 
     def get(self):
         """"""
         
-        return self._execute("""select * from Orders""", read=True)
+        return self._execute("""select * from Clients""", read=True)
 
     def _execute(self, query: str, read: bool = False):
         """"""
@@ -63,3 +73,15 @@ class Database(object):
         finally:
             connection.commit()
             connection.close()
+
+    def _connect(self):
+            """"""
+
+            try:
+                connection = sqlite3.connect(Config.DATABASE)
+                cursor = connection.cursor()
+            except sqlite3.Error as e:
+                print('database._connect |', e)
+                connection.close()
+            else:
+                return connection, cursor
--- Site/app/routes.py
+++ Site/app/routes.py
@@ -5,16 +5,13 @@ from app import app, database
 
 @app.route('/', methods=['GET'])
 def index():
-    orders = database.get()
-    return render_template('index.html', orders=orders)
+    clients = database.get()
+    return render_template('index.html', clients=clients)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        name = request.form.get('name')
-        surname = request.form.get('surname')
-        mobile_number = request.form.get('mobile_number')
-        database.add(name=name, surname=surname, mobile_number=mobile_number)
+        database.add(request.form)
         return redirect('/')
     if request.method == 'GET':
         return render_template('add.html')
